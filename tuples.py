80d7d989a87685638416df8f74b8e634f0b01fdb
Sonia Singh <sonia_singh20@hotmail.com>
2025-05-02 14:53:57 -0400

    added more info about tuples

--- tuples.py
+++ tuples.py
@@ -15,4 +15,14 @@ for i in mytuple:
  
 my_tuple=('a', 'b', 'c', 'd')
 print(my_tuple.count('a'))
-print(my_tuple.index('p'))
+print(my_tuple.index('p')) 
+
+a=(1,2,3,4,5,6,7,8,9,10)
+b=a[2:5]
+print(b)
+
+my_tuple=(0,1,2,3,4)
+i1,*i2, i3 = my_tuple
+print(i1)
+print(i2)
+print(i3)
