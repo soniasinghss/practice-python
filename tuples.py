2cea9a8825ed53bba6ffb3a4c867199a5190ad58
Sonia Singh <sonia_singh20@hotmail.com>
2025-05-02 14:26:45 -0400

    added a few lines about tuples

--- tuples.py
+++ tuples.py
@@ -1,2 +1,18 @@
-print("sonia")
-print("this is a tuple")
+#tuples are ordered immutable and allow duplicates 
+
+mytuple = "Max", 28, "Boston"
+print(mytuple)
+print(type(mytuple))
+
+item=mytuple[0]
+print(item)
+for i in mytuple:
+    print(i)
+    if "Max" in mytuple:
+        print("Max is in the tuple")
+    else:
+        print("Max is not in the tuple")
+ 
+my_tuple=('a', 'b', 'c', 'd')
+print(my_tuple.count('a'))
+print(my_tuple.index('p'))
