a8a6960a76e3cc8d09427f39ff7c9f7ac76b204c
Sonia Singh <sonia_singh20@hotmail.com>
2025-05-02 14:05:52 -0400

    added a few more lines about lists

--- lists.py
+++ lists.py
@@ -1,3 +1,14 @@
-mylist = [1,2,3,4,5,6,7,8,9]
-a=mylist[::2]
-print(a)
+
+
+list_org=["banana","cherry","apple"]
+list_cpy=list_org
+list_cpy=list_org[:] #slicing like this will also make a copy
+list_cpy.append("lemon") #same list in memory so changes to cpy are reflected in org as well
+
+print(list_cpy)
+print(list_org)
+
+mylist=[1,2,3,4,5,6]
+b=[i*i for i in mylist]
+print(mylist)
+print(b)
